00dbd5cd7e1af598e3e67103a2996fd50dd0839f
Andy Shin <sshin1@nd.edu>
2019-03-30 19:47:47 -0400

    a

--- flaskServer.py
+++ flaskServer.py
@@ -1,6 +1,6 @@
 import os
 from flask import request, Flask, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from backend.eventClasses import eventList
 from backend.userClass import userList
 
